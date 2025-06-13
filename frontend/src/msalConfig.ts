896ed27ac96d3bf4ca8db5f3e3329e6f7f158fe9
Jacob Ma <jma@ihinet.local>
2025-06-13 12:27:44 -0400

    Remove src/ from tracking

--- frontend/src/msalConfig.ts
+++
@@ -1,9 +0,0 @@
-import { Configuration } from "@azure/msal-browser";
-
-export const msalConfig: Configuration = {
-    auth: {
-        clientId: "YOUR_CLIENT_ID",
-        authority: "https://login.microsoftonline.com/Yf8cdef31-a31e-4b4a-93e4-5f571e91255a",
-        redirectUri: "http://192.168.3.93:3000", // or localhost for testing
-    },
-};
